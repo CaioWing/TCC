d8593a230bba6f9cc76a7506745bfb72ce0b4ada
CaioWing <caio@airis-tech.com>
2026-02-24 19:25:49 -0300

    Updates

--- doc/build.sh
+++ doc/build.sh
@@ -4,8 +4,6 @@
 
 MAIN="USPSC-TCC-modelo-EESC"
 BUILDDIR="build"
-LATEX="pdflatex -interaction=nonstopmode -output-directory=$BUILDDIR"
-BIBTEX="bibtex"
 
 if [ "$1" = "clean" ]; then
     rm -rf "$BUILDDIR"
@@ -16,17 +14,7 @@ fi
 
 mkdir -p "$BUILDDIR"
 
-echo "[1/4] Primeira passagem pdflatex..."
-$LATEX "$MAIN.tex"
-
-echo "[2/4] BibTeX..."
-$BIBTEX "$BUILDDIR/$MAIN"
-
-echo "[3/4] Segunda passagem pdflatex..."
-$LATEX "$MAIN.tex"
-
-echo "[4/4] Terceira passagem pdflatex..."
-$LATEX "$MAIN.tex"
+latexmk -pdf -auxdir="$BUILDDIR" -outdir="$BUILDDIR" -interaction=nonstopmode "$MAIN.tex"
 
 if [ -f "$BUILDDIR/$MAIN.pdf" ]; then
     echo ""
